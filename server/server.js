cdbf3a0a7ca1786972be2e4b87c129b232b23ec4
Mayur <mayurmahajan678910@gmail.com>
2026-06-03 20:47:20 +0530

    commited

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import employeesRouter from "./routes/employeeRoutes.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
-// import profileRouter from "./routes/profileRoutes.js";
+import leaveRouter from "./routes/leaveRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
+import payslipRouter from "./routes/payslipsRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -20,9 +22,10 @@ app.use(multer().none());
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/auth", authRouter);
 app.use("/api/employees", employeesRouter);
-// app.use("/api/profile", profileRouter);
+app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
-
+app.use("/api/leave", leaveRouter);
+app.use("/api/payslips", payslipRouter);
 
 await connectDB();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
